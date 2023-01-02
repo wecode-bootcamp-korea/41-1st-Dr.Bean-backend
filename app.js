3c5eeb98631ce435e7fcb9dbce682a9d28a2bf19
seuungkei <seuungkei@gmail.com>
2023-01-02 19:06:33 +0900

    [ADD] 초기 환경 세팅

---
+++ app.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const routes = require('./routes');
+
+const { mysqlDatabase } = require('./models/myDataSource');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(morgan('dev'));
+app.use(routes);
+
+// Health Check
+app.get('/pong', async (req, res) => {
+  res.status(200).json({ message: 'pingssss' });
+});
+
+const PORT = process.env.PORT;
+
+const start = async () => {
+  try {
+    await mysqlDatabase.initialize().then(() => {
+      console.log('Data Source has been inilialized!💡💡 ');
+    });
+    app.listen(PORT, () => console.log(`server is listening on ${PORT}🔥🔥🔥🔥🔥🔥🔥 `));
+  } catch (err) {
+    console.error('Error during Data Source initialization', err);
+    mysqlDatabase.destroy();
+  }
+};
+
+start();
