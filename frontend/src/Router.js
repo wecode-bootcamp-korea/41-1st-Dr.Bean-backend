aaa73e13cac59f0b62aa0bff864334d46afdaf04
김기윤 <ki@Kis-MacBook-Pro.local>
2023-01-02 16:10:39 +0900

    Add: 초기세팅완료

---
+++ frontend/src/Router.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+export default function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" />
+      </Routes>
+    </BrowserRouter>
+  );
+}
