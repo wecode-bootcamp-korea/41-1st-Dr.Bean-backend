389c9ffb59bbac16b32cb8d826af41ba45da6aa5
seuungkei <seuungkei@gmail.com>
2023-01-06 13:15:04 +0900

    [MODIFY] migration 수정 및 추가

--- db/migrations/20230103111809_create_items_table.sql
+++ db/migrations/20230103111809_create_items_table.sql
@@ -1,15 +1,16 @@
 -- migrate:up
 CREATE TABLE items (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(50) NOT NULL, 
+  name VARCHAR(50) NOT NULL,
   description VARCHAR(1000) NULL,
+  item_img VARCHAR(2000) NOT NULL,
   price INT NOT NULL,
   category_id INT NOT NULL,
-  item_image_id INT NOT NULL,
+  country_id INT NOT NULL,
   created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   CONSTRAINT items_category_id_fkey FOREIGN KEY (category_id) REFERENCES categories(id),
-  CONSTRAINT items_item_image_id_fkey FOREIGN KEY (item_image_id) REFERENCES item_images(id)
+  CONSTRAINT items_country_id_fkey FOREIGN KEY (country_id) REFERENCES sub_categories(id),
 );
 -- migrate:down
 DROP TABLE items;
