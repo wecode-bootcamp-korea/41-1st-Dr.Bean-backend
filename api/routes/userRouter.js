d7015e219c665e2d6f48fdcb9d330492c266f534
seuungkei <seuungkei@gmail.com>
2023-01-07 15:30:41 +0900

    [MODIFY] users 관련내용 다른 브랜치에서 수정2

--- api/routes/userRouter.js
+++ api/routes/userRouter.js
@@ -4,7 +4,7 @@ const userController = require("../controllers/userController");
 
 const router = express.Router();
 
-router.post("/singup", userController.signup);
+router.post("/signup", userController.signup);
 
 router.post("/signin", userController.signin);
 
