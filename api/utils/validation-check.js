c83ffab96fe14ac87c01f09250e6fe6eb6af61b4
김동규 <pythakyu@gmail.com>
2023-01-24 16:08:31 +0900

    [MODIFY] feature/users globalErrorHandler 적용

---
+++ api/utils/validation-check.js
@@ -0,0 +1,13 @@
+const passwordValidationCheck = (password) => {
+  const pwValidation = new RegExp("^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,20})");
+
+  if (!pwValidation.test(password)) {
+    const err = new Error("PASSWORD_IS_NOT_VALID");
+    err.statusCode = 409;
+    throw err;
+  }
+};
+
+module.exports = {
+  passwordValidationCheck,
+};
